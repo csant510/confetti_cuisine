a8e49af200d67cc787db7b414951bb01e206d701
csant510 <james.sant83@gmail.com>
2021-01-23 21:14:38 -0800

    added body parser and express.json parameters

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const express = require("express"),
     app = express();
 
 app.set("port", process.env.Port || 3000);
+app.use(express.urlencoded({
+    extended: false
+    })
+);
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Welcome To Confettii Cusine!")
