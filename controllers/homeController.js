a471f933c0597131b5d34d2218dce3388dec3eda
csant510 <james.sant83@gmail.com>
2021-01-23 21:55:54 -0800

    updated home controllers

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,13 +1,28 @@
-exports.showCourses = (req, res) => {
-    res.render("courses");
+var courses = [
+    {
+      title: "Event Driven Cakes",
+      cost: 50
+    },
+    {
+      title: "Asynchronous Artichoke",
+      cost: 25
+    },
+    {
+      title: "Object Oriented Orange Juice",
+      cost: 10
+    }
+  ];
+  
+  exports.showCourses = (req, res) => {
+    res.render("courses", {
+      offeredCourses: courses
+    });
   };
   
-exports.showSignUp = (req, res) => {
+  exports.showSignUp = (req, res) => {
     res.render("contact");
   };
-
-exports.postedSignUpForm = (req, res) => {
+  
+  exports.postedSignUpForm = (req, res) => {
     res.render("thanks");
   };
-  
-  
