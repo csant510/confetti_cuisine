80c25385867d83bd8cdab4679b5ec2168d810da2
csant510 <james.sant83@gmail.com>
2021-01-23 21:41:16 -0800

    udpated index.ejs

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,12 @@
 exports.showCourses = (req, res) => {
     res.render("courses");
   };
-  exports.showSignUp = (req, res) => {
+  
+exports.showSignUp = (req, res) => {
     res.render("contact");
   };
-  exports.postedSignUpForm = (req, res) => {
+
+exports.postedSignUpForm = (req, res) => {
     res.render("thanks");
   };
   
